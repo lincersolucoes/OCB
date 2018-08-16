1c71ecf45d30e6edb121173900f5d0987bc3390c
dontk1 <komolafetosin@gmail.com>
2018-08-16 13:48:23 +0100

    added group operator avg

--- odoo/fields.py
+++ odoo/fields.py
@@ -1151,7 +1151,7 @@ class Float(Field):
     type = 'float'
     _slots = {
         '_digits': None,                # digits argument passed to class initializer
-        'group_operator': None,
+        'group_operator': 'avg',
     }
 
     def __init__(self, string=Default, digits=Default, **kwargs):
